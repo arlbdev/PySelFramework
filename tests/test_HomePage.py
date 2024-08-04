cd087ce0a072132cb7507d9ea28e2dcb05bed800
Azi G <yeahitsazi@gmail.com>
2024-08-04 15:33:52 +0800

    Added data driven mechanism to remove hard coded data

--- tests/test_HomePage.py
+++ tests/test_HomePage.py
@@ -1,21 +1,26 @@
-from selenium.webdriver.support.select import Select
+import pytest
 from pageObjects.HomePage import HomePage
 from utilities.BaseClass import BaseClass
 
 
 class TestHomePage(BaseClass):
 
-    def test_formSubmission(self):
+    def test_formSubmission(self, getData):
 
         homePage = HomePage(self.driver)
 
-        homePage.getNameField().send_keys("Juan")
-        homePage.getEmailField().send_keys("juandelacruz@gmail.com")
+        homePage.getNameField().send_keys(getData[0])
+        homePage.getEmailField().send_keys(getData[1])
         homePage.getSampleCheckBox().click()
-        self.selectOptionByText(homePage.getGenderForm(), "Male")
+        self.selectOptionByText(homePage.getGenderForm(), getData[2])
 
         homePage.getSubmitFormBtn().click()
 
         alertText = homePage.getSuccessMessageTxt().text
 
         assert ("Success" in alertText)
+        self.driver.refresh()
+
+    @pytest.fixture(params=[("Juan", "juandelacruz@gmail.com", "Male"), ("Jane", "janedoe@gmail.com", "Female")])
+    def getData(self, request):
+        return request.param
