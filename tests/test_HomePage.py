570d5bdf4b3c3bd89b064b1b0e6c477ddbc78b14
Azi G <yeahitsazi@gmail.com>
2024-08-04 16:35:33 +0800

    Implemented logging feature into framework tests

--- tests/test_HomePage.py
+++ tests/test_HomePage.py
@@ -9,18 +9,29 @@ class TestHomePage(BaseClass):
 
     def test_formSubmission(self, getData):
 
+        log = self.getLogger()
+
         homePage = HomePage(self.driver)
 
+        log.info("Entering first name: " + getData["firstName"])
         homePage.getNameField().send_keys(getData["firstName"])
+
+        log.info("Entering email address: " + getData["email"])
         homePage.getEmailField().send_keys(getData["email"])
+
         homePage.getSampleCheckBox().click()
+
+        log.info("Choosing gender: " + getData["gender"])
         self.selectOptionByText(homePage.getGenderForm(), getData["gender"])
 
+        log.info("Submitting form...")
         homePage.getSubmitFormBtn().click()
 
         alertText = homePage.getSuccessMessageTxt().text
+        log.info("Form submitted with message: " + alertText)
 
         assert ("Success" in alertText)
+
         self.driver.refresh()
 
     @pytest.fixture(params=HomePageData.test_HomePage_data)
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -5,28 +5,41 @@ class TestOne(BaseClass):
 
     def test_e2e(self):
 
+        log = self.getLogger()
+
         homePage = HomePage(self.driver)
+        checkOutPage = homePage.clickShopItems()
 
-        checkOutPage = homePage.shopItems()
-
+        log.info("Getting all card titles...")
         cards = checkOutPage.getCardTitles()
-
         i = -1
         for card in cards:
             i += 1
             cardText = card.text
-            print(cardText)
+            log.info(cardText)
             if cardText == "Blackberry":
                 checkOutPage.getAddToCartBtn()[i].click()
 
         confirmPage = checkOutPage.clickCheckOutButton()
 
         confirmPage.getConfirmCheckoutBtn().click()
+
+        log.info("Entering country name as 'united'...")
         confirmPage.getCountryInputField().send_keys(confirmPage.countrySearchKey)
+
+        log.info("Verifying country name's existence...")
         self.verifyLinkPresence(confirmPage.countryName)
+
         confirmPage.getCountryLink().click()
+        log.info("Selecting matching country...")
+
         confirmPage.getTncCheckBox().click()
+        log.info("Checked terms and conditions...")
+
         confirmPage.getPurchaseBtn().click()
+        log.info("Confirming purchase...")
+
         textResult = confirmPage.getTextResult().text
+        log.info("Result: " + textResult)
 
         assert ("Success! Thank you!" in textResult)
